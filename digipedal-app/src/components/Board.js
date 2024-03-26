a586564a4825c4e65407ff57fb9ce1cfe927fde1
Joshua Clark <Joshrclark02@hotmail.com>
2024-03-26 14:11:53 -0500

    Draggable is working

--- digipedal-app/src/components/Board.js
+++ digipedal-app/src/components/Board.js
@@ -4,7 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle'; 
 
 import './Navbar.css';
+import "./Board.css";
 import Loading from './Loading';
+
+// drag and drop stuff
+import {DndContext} from '@dnd-kit/core';
+import Draggable from './dnd/Draggable';
+import Droppable from './dnd/Droppable';
 
 function Board( {boards} ) {
     const { id }  = useParams();
@@ -24,25 +30,66 @@ function Board( {boards} ) {
         }
         setCurrBoard(boards.find((board) => board.id == id))
     }, [id, boards, isLoading]);
-    
+
+    const [pedalsMap, setPedalsMap] = useState(new Map());
+
+    useEffect(() => {
+        let tempPedals = new Map();
+        let pedalMaxId = 0;
+        if(currBoard && currBoard.pedals){
+            currBoard.pedals.forEach((pedal) => {
+                pedal.id = pedalMaxId++;
+                tempPedals.set(pedal.id, pedal);
+            });
+        }
+        setPedalsMap(tempPedals);
+    }, [currBoard]);
+
+    console.log(currBoard)
+    console.log(pedalsMap)
+
     return (
         isLoading ? 
         <Loading /> :
-        <div>
+        <>
             <div className="navbar sticky-top d-flex justify-content-between align-items-center">
                 <a className="navbar-brand logo-container" href="/">
-                    <img src="../logo.png" className="d-inline-block align-top logo-container" alt="Digipedal Logo"/>
+                    <img src="/logo.png" className="d-inline-block align-top logo-container" alt="Digipedal Logo"/>
                 </a>
                 <div className="navbar-nav">
                     <a className="bungee-regular"> {currBoard.name} </a>
                 </div>
                 <div className="logo-container">  
                 </div>
-        </div>
-            <div className="container-fluid">
             </div>
-        </div>
+            <DndContext onDragEnd={handleDragEnd}>
+                <Droppable className="w-100 h-100">
+                    {[...pedalsMap.values()].map((pedal) => {
+                        console.log(pedal);
+                        return <Draggable id={pedal.id} x={pedal.x} y={pedal.y}><img className="pedal" src={pedal.image} key={pedal.id}/></Draggable>;
+                    })}
+                </Droppable>
+            </DndContext>
+        </>
     );
+    
+    
+    function handleDragEnd(event) {
+        console.log(event)
+        const activePedal = pedalsMap.get(event.active.id);
+        const draggedElement = document.getElementById(event.active.id);
+        console.log(draggedElement)
+        const draggedElementRect = draggedElement.getBoundingClientRect();
+        console.log(draggedElementRect)
+        if (activePedal) {
+            const updatedPedal = {
+                ...activePedal,
+                x: draggedElementRect.x,
+                y: draggedElementRect.y,
+            };
+            setPedalsMap(prev => new Map(prev).set(activePedal.id, updatedPedal));
+        }
+    }
 };
 
 
---
+++ digipedal-app/src/components/dnd/Draggable.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {useDraggable} from '@dnd-kit/core';
+import {CSS} from '@dnd-kit/utilities';
+
+
+
+function Draggable(props) {
+  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+    id: props.id,
+  });
+
+  // Within your component that receives `transform` from `useDraggable`:
+  let style = {
+    position: `absolute`,
+    left: `${props.x}px`,
+    top: `${props.y}px`,
+    transform: CSS.Translate.toString(transform),
+  }
+
+  return (
+    <button id={props.id} ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {props.children}
+    </button>
+  );
+}
+export default Draggable;
