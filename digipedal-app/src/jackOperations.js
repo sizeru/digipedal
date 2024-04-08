977dbf2ea09231c3d98108ba17884e1178408625
jay <jayjsca@gmail.com>
2024-04-08 04:06:34 -0500

    JACK APIs

--- digipedal-app/src/jackOperations.js
+++ digipedal-app/src/jackOperations.js
@@ -1,46 +1,75 @@
-
-export const getJACKPedal = async (pedal_uri) => {
-    const response = await fetch(`http://localhost:3444/pedals?pedal_uri=${pedal_uri}`);
-    if (response.ok == 200) console.log("Pedal exists in JACK with uri" + pedal_uri);
-    else if (response.ok == 404) console.log("Pedal doesn't exist in JACK with uri" + pedal_uri)
-    else console.log("Server error on JACK Pedal GET");
-    return response.ok == 200 ? 1 : 0;
-}
-
-export const updateJACKPedal = async (board_id, pedal_idx, param_vals) => {
-    const response = await fetch(`http://localhost:3444/parameter?board_id=${board_id}&?pedal_index=${pedal_idx}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(param_vals)
-    }); 
-    if (response.ok == 200) console.log("Pedal updated successfully");
-    else console.log("Server Error on JACK PUT");
-    return response.ok;
-}
-
-export const deleteJACKPedal = async (board_id, pedal_idx) => {
-    const response = await fetch(`http://localhost:3444/pedals/${board_id}/${pedal_idx}`, {
+export const deleteJACKPedals = async (board_idx) => {
+    const response = await fetch(`http://localhost:3444/board?board_index=${board_idx}`, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
         }
     });
+    if (response.ok == 200) return 1;
+    else {
+        console.log("Server error on JACK DELETE board");
+        return 0;
+    }
+}
+
+export const getActiveJACKBoard = async () => {
+    const response = await fetch(`http://localhost:3444/boards/active`);
+    if (response.ok == 200) {
+        return response.json().board_index;
+    }
+    else console.log("Server error on JACK Active Board GET");
+}
+
+export const putActiveJACKBoard = async (board_idx) => {
+    const response = await fetch(`http://localhost:3444/boards/active`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({board_index: board_idx})
+    });
+}
+
+export const addJACKPedal = async (board_idx, pedal_idx, pedal_uri, tarball) => {
+    const response = await fetch(`http://localhost:3444/board/pedal`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({board_index: board_idx, pedal_index: pedal_idx, pedal_uri: pedal_uri, tarball: tarball})
+    });
+    if (response.ok == 200) console.log("Pedal added successfully");
+    else console.log("Server error on JACK POST pedal");
+}
+
+export const changeJACKPedal = async (board_idx, pedal_idx, param_vals) => {
+    const response = await fetch(`http://localhost:3444/board/pedal`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({board_index: board_idx, pedal_index: pedal_idx, param_vals: param_vals})
+    });
+    if (response.ok == 200) console.log("Pedal updated successfully");
+    else console.log("Server error on JACK PUT pedal");
+}
+
+export const deleteJACKPedalfromBoard = async (board_idx, pedal_idx) => {
+    const response = await fetch(`http://localhost:3444/board/pedal?board_index=${board_idx}&pedal_index=${pedal_idx}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
     if (response.ok == 200) console.log("Pedal deleted successfully");
     else console.log("Server error on JACK DELETE pedal");
 }
 
-export const postJACKPedal = async (board_id, pedal_idx, pedal_uri) => {
-}
-
-export const getJACKBoard = async () => {
-    const response = await fetch(`http://localhost:3444/boards/current`);
-    if (response.ok == 200) console.log("Current Board exists in JACK" + pedal_uri);
-    else if (response.ok == 404) console.log("Current Board doesn't exist in JACK with uri" + pedal_uri)
-    else console.log("Server error on JACK Board GET");
-    return response.ok == 200 ? 1 : 0;
-}
-
-export const putJACKBoard = async () => {
+export const getJACKPedal = async (pedal_uri) => {
+    const response = await fetch(`http://localhost:3444/pedal?pedal_uri=${pedal_uri}`);
+    if (response.ok == 200) return response.ok;
+    else {
+        console.log("Server error on JACK GET pedal");
+        return 404;
+    }
 }
