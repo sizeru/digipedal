2bf5cee603ec944126dbfa3058dda1cacd3ebc0a
Cristian Cantu <cristiancant36@utexas.edu>
2024-04-07 20:52:37 -0500

    added delete confirmation modal

--- digipedal-app/src/components/SplitDropDown.js
+++ digipedal-app/src/components/SplitDropDown.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { SplitButton, Dropdown } from 'react-bootstrap';
+import { SplitButton, Dropdown, Modal, Button} from 'react-bootstrap';
 import { deleteBoard, getBoardById } from '../firebaseOperations';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,6 +8,7 @@ function SplitDropDown(id, setLoading) {
   const [selectedItem, setSelectedItem] = useState('...');
   const [isRenaming, setIsRenaming] = useState(false);
   const [newName, setNewName] = useState('');
+  const [showModal, setShowModal] = useState(false); 
 
   // Options for the dropdown
   const options = ['Rename', 'Delete'];
@@ -17,9 +18,11 @@ function SplitDropDown(id, setLoading) {
     setSelectedItem(option);
     console.log(`You selected: ${option}`);
     if (option == 'Delete') {
+      setShowModal(true);
       setLoading(true)
       await deleteBoard(id.toString());
-      setLoading(true);
+      setLoading(false);
+      setShowModal(false);
     }
     if (option == 'Rename') {
         setIsRenaming(true);
@@ -27,9 +30,6 @@ function SplitDropDown(id, setLoading) {
         setSelectedItem(option);
         setIsRenaming(false);
     }
-      
-    
-       
       
 
   };
@@ -42,6 +42,7 @@ function SplitDropDown(id, setLoading) {
       setNewName(''); // Reset the input field
     }
   };
+  
 
   return (
     <div>
@@ -58,6 +59,21 @@ function SplitDropDown(id, setLoading) {
       ))}
     </SplitButton>
 
+    <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Delete</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete this item?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={handleSelect}>
+            Delete
+          </Button>
+        </Modal.Footer>
+    </Modal>
+
     {isRenaming && (
         <input
           type="text"
@@ -68,6 +84,7 @@ function SplitDropDown(id, setLoading) {
           autoFocus
         />
       )}
+
     </div>
   );
 }
