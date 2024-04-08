004daf4416fb334a10767d428f49cbfffeb51ded
sizeru <gaafts@gmail.com>
2024-04-07 23:31:33 -0500

    Implement delete

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,12 +1,28 @@
 use std::{
-    convert::Infallible, env, fs, io, net::SocketAddr, path::PathBuf
+    borrow::BorrowMut, collections::{HashMap, LinkedList}, convert::Infallible, env, fs, intrinsics::needs_drop, io, net::SocketAddr, path::PathBuf, str::Utf8Error, sync::{Arc, OnceLock}
 };
-use http_body_util::Full;
-use hyper::{body::Bytes, server::conn::http1, service::service_fn, Request, Response};
+use http_body_util::{BodyExt, Collected, Full};
+use hyper::{
+    body::Bytes,
+    server::conn::http1,
+    service::service_fn,
+    Method,
+    Request,
+    Response
+};
 use hyper_util::rt::TokioIo;
+use json::JsonValue;
 use log::{info, error};
 use daemonize::Daemonize;
-use tokio::net::TcpListener;
+use tokio::{
+    net::TcpListener, process::Command, sync::{Mutex, RwLock}
+};
+use serde::ser;
+
+static BOARDS: OnceLock<RwLock<Vec<Board>>> = OnceLock::new();
+fn boards() -> &'static RwLock<Vec<Board>> {
+    BOARDS.get_or_init(|| RwLock::new(Vec::new()))
+}
 
 #[derive(Debug)]
 enum Error {
@@ -18,6 +34,11 @@ enum Error {
     DaemonLaunch(daemonize::Error),
     TcpBind(io::Error),
     Tcp(io::Error),
+    BodyUnreadable,
+    BodyNotUtf8(Utf8Error),
+    BodynotJSON,
+    QueryNotPresent,
+    BoardIndexOOB(i32),
 }
 
 #[derive(Debug)]
@@ -155,12 +176,33 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
+struct Pedal {
+    uri: String,
+    config_values: HashMap<String, String>,
+    input_port: String,
+    output_port: String,
+}
+
+struct Board {
+    pedals: LinkedList<Pedal>
+}
+
+impl Board {
+    fn new() -> Self {
+        Self {
+            pedals: LinkedList::new(),
+        }
+    }
+}
+
 // Run the jack server. This function will often never return
 #[tokio::main]
 async fn run_server(config: Config) -> Result<(), Error> {
     info!("Starting server!");
     let listener = TcpListener::bind(config.addr).await.map_err(|e| Error::TcpBind(e))?;
     info!("Bound to address: {}", config.addr);
+
+    // let boards: Arc<Mutex<Vec<Board>>> = Arc::new(Mutex::new(Vec::new()));
     loop {
         let (stream, _) = listener.accept().await.map_err(|e| Error::Tcp(e))?;
         let io = TokioIo::new(stream);
@@ -177,8 +219,96 @@ async fn run_server(config: Config) -> Result<(), Error> {
 }
 
 async fn process_req(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    let response = format!("You requested: {}\n", req.uri());
-    return Ok(Response::new(Full::new(Bytes::from(response))));
+    info!("Received a request for: {}", req.uri());
+    let response = match (req.method(), req.uri().path()) {
+        (&Method::DELETE, "/board") => delete_pedals(req).await,
+        (&Method::GET, "/board/active") => async_todo().await,
+        (&Method::PUT, "/board/active") => async_todo().await,
+        (&Method::POST, "/board/pedal") => async_todo().await,
+        (&Method::PUT, "/board/pedal") => async_todo().await,
+        (&Method::DELETE, "/board/pedal") => async_todo().await,
+        (&Method::GET, "/pedal") => async_todo().await,
+        (_, _) => not_supported().await,
+    };
+    return Ok(response.unwrap());
+    // return Ok(Response::new(Full::new(Bytes::from(response))));
+}
+
+async fn async_todo() -> Result<Response<Full<Bytes>>, Error> {
+    return Ok(Response::new(Full::from("TODO")));
+}
+
+async fn not_supported() -> Result<Response<Full<Bytes>>, Error> {
+    return Ok(Response::new(Full::from("This uri is not supported")));
+}
+
+fn insert_at(l: &mut LinkedList<u8>, idx: usize, val: u8) {
+    let mut tail = l.split_off(idx);
+    l.push_back(val);
+    l.append(&mut tail);
+}
+
+// Delete all pedals from a specified board
+async fn delete_pedals(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Error> {
+    let queries = get_queries(req);
+    let board_index = queries
+        .get("board_index")
+        .map(|b_idx| b_idx.parse().unwrap_or(-1))
+        .unwrap_or(-1);
+    if board_index == -1 {
+        return Err(Error::BoardIndexOOB(board_index));
+    } else {
+        let mut boards = boards()
+            .write()
+            .await;
+        let board = boards.get_mut(board_index as usize).unwrap();
+        let pedals = board.pedals.borrow_mut();
+        let mut iter = pedals.iter_mut();
+        let mut cur_pedal = iter.next().unwrap();
+        let mut next = iter.next();
+        loop {
+            if next.is_none() { // Break if last
+                break;
+            }
+            let next_pedal = next.unwrap();
+            let command = format!("jack_disconnect {} {}", cur_pedal.output_port, next_pedal.input_port);
+            Command::new(command);
+
+            // Next iteration
+            cur_pedal = next_pedal;
+            next = iter.next();
+        }
+
+        let input = pedals.pop_front().unwrap();
+        let output = pedals.pop_back().unwrap();
+        let mut ll = LinkedList::new();
+        ll.push_front(input);
+        ll.push_back(output);
+    }
+
+    // info!("Succesfully parsed json: {json:#?}");
+    return Ok(Response::new(Full::new(Bytes::from(""))));
+}
+
+fn get_queries(req: Request<hyper::body::Incoming>) -> HashMap<String, String> {
+    req.uri()
+        .query()
+        .map(|v| {
+            url::form_urlencoded::parse(v.as_bytes())
+                .into_owned()
+                .collect()
+        }) .unwrap_or_else(HashMap::new)
+}
+
+async fn get_json_from_body(body: hyper::body::Incoming) -> Result<JsonValue, Error> {
+    let body = body
+        .collect()
+        .await
+        .map_err(|e| Error::BodyUnreadable)?
+        .to_bytes();
+    let contents = std::str::from_utf8(body.as_ref())
+        .map_err(|e| Error::BodyNotUtf8(e))?;
+    json::parse(contents).map_err(|e| Error::BodynotJSON)
 }
 
 /*
@@ -186,40 +316,3 @@ async fn process_req(req: Request<hyper::body::Incoming>) -> Result<Response<Ful
  *  - Server creation / daemonization
  *  - Jack routing based on server arguments
  */
-
-// use std::convert::Infallible;
-// use std::net::SocketAddr;
-// use http_body_util::Full;
-// use hyper::body::Bytes;
-// use hyper::server::conn::http1;
-// use hyper::service::service_fn;
-// use hyper::{Request, Response};
-// use hyper_util::rt::TokioIo;
-// use tokio::net::TcpListener;
-// use std::sync::Arc;
-
-// // Respond to the request
-// async fn process_req(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-// 	Ok(Response::new(Full::new(Bytes::from("Ok"))))
-// }
-
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-// 	// TODO: Make this be read from the config
-// 	let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-// 	let listener = TcpListener::bind(addr).await?;
-
-// 	loop {
-// 		let (stream, _) = listener.accept().await?;
-// 		let io = TokioIo::new(stream); // tokio to hyper trait adapter
-
-// 		tokio::task::spawn(async move {
-// 			if let Err(err) = http1::Builder::new()
-// 				.serve_connection(io, service_fn(process_req))
-// 				.await
-// 			{
-// 				eprintln!("Error serving connection: {:?}", err);
-// 			}
-// 		});
-// 	}
-// }
