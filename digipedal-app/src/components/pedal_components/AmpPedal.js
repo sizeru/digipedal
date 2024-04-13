c2eb12daaecc3ed75efd59bd595fdc2fd6134d0d
Joshua Clark <Joshrclark02@hotmail.com>
2024-04-13 16:47:14 -0500

    Add number indicator to knobs for current value
    - Made incrementor also more precise and able to go backwards

--- digipedal-app/src/components/pedal_components/AmpPedal.js
+++ digipedal-app/src/components/pedal_components/AmpPedal.js
@@ -17,16 +17,27 @@ function AmpPedal({width, height, isStatic, toggled, param_vals, togglePedal, de
 
   function increment_amplification(event) {
     console.log("increment_amplification")
+    console.log(event)
+    if(event == null || event.activatorEvent == null){
+      console.log("returning early since: event == null || event.activatorEvent == null");
+      return 
+    }
     if(isStatic){
       console.log("not doing anything cause static")
       return ;
     }
-    console.log(event)
-    let newAmplification = amplification + (maxAmplifcation - minAmplifcation) / 20
-    if(newAmplification > maxAmplifcation){
-      newAmplification = minAmplifcation
+
+    let increment_amount = (maxAmplifcation - minAmplifcation) / 100;
+    if(event.activatorEvent.ctrlKey){
+      increment_amount *= -1;
     }
-    setAmplification(newAmplification + .1)
+    let newAmplification = amplification + increment_amount;
+    if(newAmplification > maxAmplifcation){
+      newAmplification = minAmplifcation;
+    } else if (newAmplification < minAmplifcation){
+      newAmplification = maxAmplifcation;
+    }
+    setAmplification(newAmplification);
   }
 
   let style = {
@@ -37,7 +48,7 @@ function AmpPedal({width, height, isStatic, toggled, param_vals, togglePedal, de
   let svg_output = (
     <svg width={width} height={height + height/5} viewBox={`0 0 ${width} ${height + height / 5}`} fill="none" xmlns="http://www.w3.org/2000/svg" style={style}>
         <rect width={width} height={height} rx="1" fill="#D9D9D9"/>
-        <Knob x={width * .5} y={height * .40} width={width * .40} rotation={amplifcationRotation} text="Amplification" isStatic={isStatic} increment={increment_amplification}/>
+        <Knob x={width * .5} y={height * .40} width={width * .40} rotation={amplifcationRotation} text="Amplification" isStatic={isStatic} increment={increment_amplification} number={amplification}/>
         <PedalBottom width={width} height={height/5} startHeight={height} toggled={toggled} togglePedal={togglePedal} deletePedal={deletePedal} showInfoModal={showInfoModal}/>
     </svg>
   );
--- digipedal-app/src/components/pedal_components/Knob.js
+++ digipedal-app/src/components/pedal_components/Knob.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 
-function Knob({x, y, width, isStatic, rotation, text, style, increment}) {
+function Knob({x, y, width, isStatic, rotation, text, style, increment, number}) {
     let text_height = Math.round(width * 3/ text.length)
 
     style = {
@@ -12,6 +12,7 @@ function Knob({x, y, width, isStatic, rotation, text, style, increment}) {
 
     return <>
         <circle cx={x} cy={y} r={width} fill="#5BB7B3" onClick={increment}/>
+        <text x={x} y={y} fontFamily="BUNGEE" fontSize={width*3/4} fill="black" dominant-baseline="middle" text-anchor="middle" onClick={increment} opacity="25%">{Number( number.toPrecision(3) )}</text>
         <path d={`M${x} ${y}L${x} ${y-width}`} stroke="black" stroke-width="2" stroke-linecap="round" style={style} onClick={increment}/> 
         <text x={x} y={y+width+text_height} fontFamily="BUNGEE" fontSize={text_height} fill="black" dominant-baseline="middle" text-anchor="middle">{text}</text>
     </>;
